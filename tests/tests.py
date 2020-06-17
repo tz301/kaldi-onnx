bf1233142eedcd858607667e0ecf1472735fb838
jasontu <jasontu@wezhuiyi.com>
2020-06-17 19:56:28 +0800

    命名调整, 格式调整, 增加typing hint

--- tests/tests.py
+++ tests/tests.py
@@ -21,36 +21,43 @@ class ConverterTest(unittest.TestCase):
     __data_dir: data dir.
   """
 
-  def setUp(self):
+  def setUp(self) -> None:
     """Share variables."""
     self.__data_dir = Path(__file__).parent / 'data'
 
-  def test_model1(self):
+  def test_model1(self) -> None:
     """Test model in kaldi/egs/swbd/s5c/local/chain/tuning/run_tdnn_7p.sh"""
     max_err = _test_one_model(self.__data_dir / 'model1', 32, 32)
     self.assertLess(max_err, 1e-5, 'model1 inference error.')
 
-  def test_model2(self):
+  def test_model2(self) -> None:
     """Test model in kaldi/egs/swbd/s5c/local/chain/tuning/run_tdnn_7q.sh"""
     max_err = _test_one_model(self.__data_dir / 'model2', 34, 34)
     self.assertLess(max_err, 1e-5, 'model2 inference error.')
 
 
-def _test_one_model(model_dir, left_context, right_context):
+def _test_one_model(mdl_dir: Path,
+                    left_context: int,
+                    right_context: int
+                    ) -> float:
   """Convert one model and check output.
 
   Args:
-    model_dir: model dir for test.
+    mdl_dir: model dir for test.
     left_context: left context of kaldi model.
     right_context: right context of kaldi model.
 
   Returns:
-    max err between tensorflow pb output and kaldi output.
+    max err percent between tensorflow pb output and kaldi output.
   """
+  feat_input = np.loadtxt(str(mdl_dir / 'input.txt'), dtype=np.float32)
+  feat_ivector = np.loadtxt(str(mdl_dir / 'ivector.txt'), dtype=np.float32)
+  kaldi_output = np.loadtxt(str(mdl_dir / 'output.txt'), dtype=np.float32)
+  feed_dict = {'input:0': feat_input, 'ivector:0': feat_ivector}
+
   with TemporaryDirectory() as tmp_dir:
-    kaldi_model_file = model_dir / 'final.txt'
     pb_file = Path(tmp_dir) / 'tf.pb'
-    converter = Converter(kaldi_model_file, left_context, right_context)
+    converter = Converter(mdl_dir / 'final.txt', left_context, right_context)
     converter.convert('tf', pb_file)
 
     tf.compat.v1.reset_default_graph()
@@ -60,13 +67,9 @@ def _test_one_model(model_dir, left_context, right_context):
         graph_def.ParseFromString(pb_file.read())
         tf.import_graph_def(graph_def, name="")
 
-      feat_input = np.loadtxt(model_dir / 'input.txt', dtype=np.float32)
-      feat_ivector = np.loadtxt(model_dir / 'ivector.txt', dtype=np.float32)
-      feed_dict = {'input:0': feat_input, 'ivector:0': feat_ivector}
       out_tensor = session.graph.get_tensor_by_name('output.affine:0')
       output = session.run(out_tensor, feed_dict)
 
-  kaldi_output = np.loadtxt(model_dir / 'output.txt', dtype=np.float32)
   return np.amax(np.absolute(np.subtract(output, kaldi_output)))
 
 
